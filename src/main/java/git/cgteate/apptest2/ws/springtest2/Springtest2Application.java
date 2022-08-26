dfd16e950567931a1e83d5e1859f49bbaec3287a
Calvin Teater <laughingglint3@gmail.com>
2022-08-26 11:15:58 -0500

    beginning setup

---
+++ src/main/java/git/cgteate/apptest2/ws/springtest2/Springtest2Application.java
@@ -0,0 +1,13 @@
+package git.cgteate.apptest2.ws.springtest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springtest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springtest2Application.class, args);
+	}
+
+}
---
+++ src/test/java/git/cgteate/apptest2/ws/springtest2/Springtest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package git.cgteate.apptest2.ws.springtest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springtest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
