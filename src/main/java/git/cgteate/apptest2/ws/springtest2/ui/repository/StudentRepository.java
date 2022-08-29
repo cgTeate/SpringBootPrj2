c2de99dd4d76e8b421ff571c24ba55d116e83784
Calvin Teater <laughingglint3@gmail.com>
2022-08-29 14:24:23 -0500

    queries from method name

--- src/main/java/git/cgteate/apptest2/ws/springtest2/ui/repository/StudentRepository.java
+++ src/main/java/git/cgteate/apptest2/ws/springtest2/ui/repository/StudentRepository.java
@@ -1,9 +1,11 @@
 package git.cgteate.apptest2.ws.springtest2.ui.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import git.cgteate.apptest2.ws.springtest2.ui.model.Student;
 
 public interface StudentRepository extends MongoRepository<Student, String>{
-    
+    Optional<Student> findStudentByEmail(String email);
 }
