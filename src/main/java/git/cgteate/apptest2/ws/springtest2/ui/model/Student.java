738ea473ead20607e96da52c96bfda6302b48c1b
Calvin Teater <laughingglint3@gmail.com>
2022-08-29 12:25:28 -0500

    create class into document

--- src/main/java/git/cgteate/apptest2/ws/springtest2/ui/model/Student.java
+++ src/main/java/git/cgteate/apptest2/ws/springtest2/ui/model/Student.java
@@ -4,10 +4,16 @@ import java.math.BigDecimal;
 import java.time.ZonedDateTime;
 import java.util.List;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import lombok.Data;
 
 @Data
+@Document()
 public class Student {
+    @Id
+    private String id;
     private String firstName;
     private String lastName;
     private String email;
