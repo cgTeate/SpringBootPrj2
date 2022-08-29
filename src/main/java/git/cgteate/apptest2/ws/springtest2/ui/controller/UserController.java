893e3d3d9a99bf0a2ef2985b7cadeb78a9f7d0c6
Calvin Teater <laughingglint3@gmail.com>
2022-08-29 09:21:25 -0500

    add update method

--- src/main/java/git/cgteate/apptest2/ws/springtest2/ui/controller/UserController.java
+++ src/main/java/git/cgteate/apptest2/ws/springtest2/ui/controller/UserController.java
@@ -7,10 +7,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.DeleteMapping;
+
 /* 
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;*/
 import org.springframework.web.bind.annotation.RestController;
 
@@ -72,14 +73,17 @@ public class UserController {
             return ResponseEntity.ok("The user " + id + " does not exist");
     }
     }
-    /*
 
-    @PutMapping
-    public String updateUser() {
-        return "update user was called";
+    @PutMapping("{id}")
+    public ResponseEntity updateUserById(@PathVariable String id, @RequestBody User user) { 
+        
+        User userToUpdate = this.userRepository.findById(id).get();
+            userToUpdate.setName(user.getName());
+            userToUpdate.setDescription(user.getDescription());
+            return ResponseEntity.ok(this.userRepository.save(userToUpdate));
     }
 
-    
-    } */
+
+
     
 }
