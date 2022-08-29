a05aa7f2460c96de955a1f2d0ccab04d33b563d9
Calvin Teater <laughingglint3@gmail.com>
2022-08-29 14:03:38 -0500

    mongo template query

--- src/main/java/git/cgteate/apptest2/ws/springtest2/Springtest2Application.java
+++ src/main/java/git/cgteate/apptest2/ws/springtest2/Springtest2Application.java
@@ -8,6 +8,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
 import git.cgteate.apptest2.ws.springtest2.ui.model.Address;
 import git.cgteate.apptest2.ws.springtest2.ui.model.Gender;
@@ -22,25 +25,44 @@ public class Springtest2Application {
 	}
 
 	@Bean
-	CommandLineRunner runner(StudentRepository repository){
+	CommandLineRunner runner(
+		StudentRepository repository, MongoTemplate mongoTemplate){
 		return args -> {
 			Address address = new Address(
 				"England",
 				"London",
 				"NE9"
 			);
-
+			
+			String email = "jahmed@gmail.com";
 			Student student = new Student(
 				"Jamila",
 				"Ahmed",
-				"jahmed@gmail.com",
+				email,
 				Gender.FEMALE,
 				address,
 				List.of("Computer Science", "Mathematics"),
 				BigDecimal.TEN,
 				LocalDateTime.now()
 			);
-			repository.insert(student);
+
+			Query query = new Query();
+			query.addCriteria(Criteria.where("email").is(email));
+
+			List<Student> students = mongoTemplate.find(query, Student.class);
+
+			if(students.size() > 1){
+				throw new IllegalStateException("found many students with email " + email);
+			}
+
+			if(students.isEmpty()){
+				System.out.println("Inserting student " + student);
+				repository.insert(student);
+			} else{
+				System.out.println(student + " already exists");
+			}
+
+			
 		};
 	}
 
