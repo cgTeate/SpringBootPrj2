8a324dcb75642aa96b05f110e9fe534de063c258
Calvin Teater <laughingglint3@gmail.com>
2022-08-29 12:21:21 -0500

    define student model

---
+++ src/main/java/git/cgteate/apptest2/ws/springtest2/ui/model/Address.java
@@ -0,0 +1,11 @@
+package git.cgteate.apptest2.ws.springtest2.ui.model;
+
+import lombok.Data;
+
+@Data
+public class Address {
+    private String country;
+    private String city;
+    private String postCode;
+
+}
